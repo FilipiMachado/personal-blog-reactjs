342ab8585d378a46995ac670cc9cd76b01d898e6
filipi <filipisee@hotmail.com>
2022-03-17 20:25:20 -0300

    adding router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
+    </Router>
   )
 }
 
