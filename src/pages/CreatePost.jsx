4d6a9879b9d5b3f346383c30e3fa6c95408fc402
filipi <filipisee@hotmail.com>
2022-03-17 22:01:36 -0300

    structuring create post page

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function CreatePost() {
+  const [title, setTitle] = useState('');
+  const [postText, setPostText] = useState('');
+
   return (
     <div className="create-post-page">
       <div className="container">
         <h1>Criar Post</h1>
         <div className="input-group">
           <label>Título: </label>
-          <input placeholder="Coloque um título" />
+          <input placeholder="Coloque um título" onChange={(event) => {
+            setTitle(event.target.value);
+          }}/>
         </div>
         <div className="input-group">
           <label>Post: </label>
-          <textarea placeholder="Escreva seu texto aqui"/>
+          <textarea placeholder="Escreva seu texto aqui" onChange={(event) => {
+            setPostText(event.target.value);
+          }}/>
         </div>
         <button>Enviar Post</button>
       </div>
