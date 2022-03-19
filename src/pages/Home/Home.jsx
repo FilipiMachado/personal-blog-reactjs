c2f813a166b3616c9951e34a06ba69da8f3dca86
filipi <filipisee@hotmail.com>
2022-03-19 00:03:11 -0300

    styling home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,52 +1,58 @@
 /* React */
-import React, { useState, useEffect  } from 'react';
+import React, { useState, useEffect } from "react";
 /* Firebase */
-import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore';
-import { db, auth } from '../../firebase-config';
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { db, auth } from "../../firebase-config";
 /* Styles */
 import "./styles.css";
 
-function Home( isAuth ) {
+function Home(isAuth) {
   const [postLists, setPostLists] = useState([]);
-  const postsCollectionRef = collection(db, 'posts');
+  const postsCollectionRef = collection(db, "posts");
 
   const deletePost = async (id) => {
-    const postDoc = doc(db, 'posts', id);
+    const postDoc = doc(db, "posts", id);
     await deleteDoc(postDoc);
-  }
+  };
 
   useEffect(() => {
     const getPosts = async () => {
       const data = await getDocs(postsCollectionRef);
-      setPostLists(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+      setPostLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
 
     getPosts();
   }, [deletePost]);
   return (
     <div className="home-page">
-      { postLists.map((post) => {
+      {postLists.map((post) => {
         return (
           <div className="post">
             <div className="post-header">
               <div className="post-header__title">
-                <h1>{ post.title }</h1>
+                <span className="post-text__title">{post.title}</span>
               </div>
               <div className="delete-post">
                 {isAuth && (
-                  <button onClick={() => {deletePost(post.id)}}>&#128465; </button>
-                ) }
+                  <button
+                    onClick={() => {
+                      deletePost(post.id);
+                    }}
+                  >
+                    &#128465;{" "}
+                  </button>
+                )}
               </div>
             </div>
             <div className="post-text__container">
-              { post.postText }
+              <span className="post-text__desc">{post.postText}</span>
             </div>
-            <h3>@{ post.author.name }</h3>
+            <span className="post-text__author">Autor: {post.author.name}</span>
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
 export default Home;
